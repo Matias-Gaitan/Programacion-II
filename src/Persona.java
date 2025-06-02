c257e40176eec2b351edcc5e2a1edefbf7242ffa
Matias-Gaitan <matiasgaitan370@gmail.com>
2025-06-02 19:48:05 -0300

    Add files via upload

---
+++ src/Persona.java
@@ -0,0 +1,8 @@
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String numeroIdentificacion;
+    private int edad;
+    private String direccion;
+    private String telefono;
+}
